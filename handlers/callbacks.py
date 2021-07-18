9a9b43f5484b0d9925aa7967ca08b3b66106f749
zharyqac <87602126+zharyqac@users.noreply.github.com>
2021-07-18 19:25:53 +0600

    Update callbacks.py

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -1,3 +1,4 @@
+from aiogram.utils.markdown import fmt
 from aiogram import types
 from dispatcher import dp
 import config
@@ -8,11 +9,12 @@ async def start_command(message: types.Message):
 	"""
 		Команда /start с кнопками
 	"""
-	# Кнопки
-	button = types.KeyboardButton(text="Авторизация 🔐")
-	markup = types.ReplyKeyboardMarkup(resize_keyboard=True).add(button)
-
-	text = f"""<i><b><a href="t.me/{message.from_user.username}">{message.from_user.first_name}</a></b></i>, чем могу помочь?"""
+	# --- Кнопки ---
+	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+	button = ["Авторизация 🔐"]
+	markup.add(*button)
+	
+	text = fmt.text(fmt.hbold(fmt.hitalic(fmt.hlink(message.from_user.first_name, f"t.me/{message.from_user.username}"))), "чем могу помочь?")
 	await message.bot.send_message(message.chat.id, text, reply_markup=markup)
 
 @dp.message_handler(lambda message: message.text == "Авторизация 🔐")
@@ -24,27 +26,13 @@ async def auth_command(message: types.Message):
 	data = message.text.split()
 
 	if len(data) == 4:
-		db.auth_user(
-			first_name=data[1],
-			last_name=data[2],
-			password=data[3]
-		)
+		db.auth_user(first_name=data[1], last_name=data[2], password=data[3])
 		await message.answer("Ваши данные успешно сохранены!")
 
 	else:
-		remove_button = types.ReplyKeyboardRemove()
-		await message.bot.send_message(
-			message.chat.id,
-			"<b>Пример:</b>\n <i>/reg Имя Фамилия Пароль</i>",
-			reply_markup=remove_button
-		)
+		text = fmt.text(fmt.hbold("Пример:"), fmt.hitalic("/reg Имя Фамилия Пароль"))
+		await message.bot.send_message(message.chat.id, "<b>Пример:</b>\n <i>/reg Имя Фамилия Пароль</i>", reply_markup=types.ReplyKeyboardRemove())
 
-@dp.message_handler(commands=["get_app"])
-async def get_app_command(message: types.Message):
-	await message.bot.send_message(
-		message.chat.id,
-		""
-	)
 
 @dp.message_handler(content_types=["text"])
 async def random_text(message: types.Message):
